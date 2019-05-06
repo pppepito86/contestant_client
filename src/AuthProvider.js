aa4d81da921ef0380b186f2ec93ac54a61dfaa36
Petar Petrov <pppepito86@gmail.com>
2019-05-06 18:06:36 +0300

    redirect to /login if not authenticated

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -3,29 +3,30 @@ import axios from 'axios';
 
 class AuthProvider extends React.Component {
 
-    componentWillMount() {
-        axios.interceptors.request.use(function (config) {
-            const token = localStorage.getItem('token1');
-            if (!config.headers.Authorization && token) config.headers.Authorization = `Bearer ${token}`;
-            return config;
-        });
+  componentWillMount() {
+    axios.interceptors.request.use(function (config) {
+      const token = localStorage.getItem('token1');
+      if (!config.headers.Authorization && token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
 
-        axios.interceptors.response.use(function (response) {
-            return response;
-        }, function (error) {
-            if (401 === error.response.status) {
-              localStorage.removeItem('token1');
-              window.location.href = '/login'
-          } else {
-              return Promise.reject(error);
-          }
-      });
+    axios.interceptors.response.use(function (response) {
+      return response;
+    }, function (error) {
+      if (401 === error.response.status) {
+        localStorage.removeItem('token1');
+        window.location.href = '/login'
+      } else {
+        return Promise.reject(error);
+      }
+    });
+  }
 
-    }
-
-    render() {
-        return this.props.children
-    }
+  render() {
+    return this.props.children
+  }
 }
 
 export default AuthProvider;
--- src/components/Layout/LayoutRoute.js
+++ src/components/Layout/LayoutRoute.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { EmptyLayout } from 'components/Layout';
+import AuthPage from 'pages/AuthPage';
 
-const LayoutRoute = ({ component: Component, layout: Layout, ...rest }) => (
-  <Route
-    {...rest}
+function layoutWithAuth(Component, Layout, rest) {
+  if (!localStorage.getItem('token1')) { 
+    Component=AuthPage;
+    Layout=EmptyLayout;
+  }
+  return <Route {...rest}
     render={props => (
       <Layout>
         <Component {...props} />
       </Layout>
     )}
   />
+}
+
+const LayoutRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  layoutWithAuth(Component, Layout, rest)
 );
 
 export default LayoutRoute;
