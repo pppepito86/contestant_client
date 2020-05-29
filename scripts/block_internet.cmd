d5a9b84ef92a74efa9780d2ca59ae802260bced9
Petar Petrov <pppepito86@gmail.com>
2020-05-30 00:31:38 +0300

    improve script

--- scripts/block_internet.cmd
+++ scripts/block_internet.cmd
@@ -2,18 +2,24 @@
 
 if "%1"=="block" (
 	echo blocking
-	netsh advfirewall export "C:\rules.wfw"
-	netsh advfirewall firewall del rule all
+	netsh advfirewall export "C:\Windows\System32\drivers\etc\rules.wfw"
+	netsh advfirewall firewall set rule all new enable=no
 	netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 	netsh advfirewall firewall add rule name="AllowDNS" program="%SystemRoot%\system32\svchost.exe" dir=out action=allow protocol=UDP remoteport=53
 	netsh advfirewall firewall add rule name="AllowPeshoorg" dir=out action=allow remoteip=85.130.26.176	
 ) else (
 	if "%1"=="unblock" (
 		echo unblocking
-		netsh advfirewall firewall del rule all
-		netsh advfirewall import "C:\rules.wfw"
 		netsh advfirewall set allprofiles firewallpolicy blockinbound,allowoutbound
+		if exist "C:\Windows\System32\drivers\etc\rules.wfw" (
+			netsh advfirewall firewall del rule all
+			netsh advfirewall import "C:\Windows\System32\drivers\etc\rules.wfw"
+		) else (
+			netsh advfirewall firewall del rule name="AllowDNS"
+			netsh advfirewall firewall del rule name="AllowPeshoorg"
+			netsh advfirewall firewall set rule all new enable=yes
+		)
 	) else (
-		echo "Call 'block_internet.cmd [block|unblock]'"
+		echo Call 'block_internet.cmd [block^|unblock]'
 	)
 )
