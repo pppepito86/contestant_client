1c3f6b89d178b7c4852b1586e6bb9940572faf89
Petar Petrov <pppepito86@gmail.com>
2020-11-18 02:40:53 +0200

    improve if already blocked

--- scripts/block_internet.cmd
+++ scripts/block_internet.cmd
@@ -1,17 +1,21 @@
 @echo off
 
 if "%1"=="block" (
-	echo blocking
-	netsh advfirewall export "C:\Windows\System32\drivers\etc\rules.wfw"
-	netsh advfirewall firewall set rule all new enable=no
-	netsh advfirewall firewall set rule group="Core Networking" new enable=yes
-	netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
-	netsh advfirewall firewall add rule name="AllowDNS" program="%SystemRoot%\system32\svchost.exe" dir=out action=allow protocol=UDP remoteport=53
-	netsh advfirewall firewall add rule name="AllowIP1" dir=out action=allow remoteip=18.158.226.30
-	netsh advfirewall firewall add rule name="AllowIP2" dir=out action=allow remoteip=18.159.251.163
-	netsh advfirewall firewall add rule name="AllowIP3" dir=out action=allow remoteip=18.192.252.174
-	netsh advfirewall firewall add rule name="AllowIP4" dir=out action=allow remoteip=18.198.128.31
-	netsh advfirewall firewall add rule name="AllowIP5" dir=out action=allow remoteip=52.58.90.28
+	if exist "C:\Windows\System32\drivers\etc\rules.wfw" (
+		echo alredy blocked
+	) else (
+		echo blocking
+		netsh advfirewall export "C:\Windows\System32\drivers\etc\rules.wfw"
+		netsh advfirewall firewall set rule all new enable=no
+		netsh advfirewall firewall set rule group="Core Networking" new enable=yes
+		netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
+		netsh advfirewall firewall add rule name="AllowDNS" program="%SystemRoot%\system32\svchost.exe" dir=out action=allow protocol=UDP remoteport=53
+		netsh advfirewall firewall add rule name="AllowIP1" dir=out action=allow remoteip=18.158.226.30
+		netsh advfirewall firewall add rule name="AllowIP2" dir=out action=allow remoteip=18.159.251.163
+		netsh advfirewall firewall add rule name="AllowIP3" dir=out action=allow remoteip=18.192.252.174
+		netsh advfirewall firewall add rule name="AllowIP4" dir=out action=allow remoteip=18.198.128.31
+		netsh advfirewall firewall add rule name="AllowIP5" dir=out action=allow remoteip=52.58.90.28
+	)
 ) else (
 	if "%1"=="unblock" (
 		echo unblocking
