70376ab4a7fef511040d832759e446448c6509ba
Petar Petrov <pppepito86@gmail.com>
2021-01-18 16:56:08 +0200

    update ips

--- scripts/block_internet.cmd
+++ scripts/block_internet.cmd
@@ -10,11 +10,9 @@ if "%1"=="block" (
 		netsh advfirewall firewall set rule group="Core Networking" new enable=yes
 		netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 		netsh advfirewall firewall add rule name="AllowDNS" program="%SystemRoot%\system32\svchost.exe" dir=out action=allow protocol=UDP remoteport=53
-		netsh advfirewall firewall add rule name="AllowIP1" dir=out action=allow remoteip=18.158.226.30
-		netsh advfirewall firewall add rule name="AllowIP2" dir=out action=allow remoteip=18.159.251.163
-		netsh advfirewall firewall add rule name="AllowIP3" dir=out action=allow remoteip=18.192.252.174
-		netsh advfirewall firewall add rule name="AllowIP4" dir=out action=allow remoteip=18.198.128.31
-		netsh advfirewall firewall add rule name="AllowIP5" dir=out action=allow remoteip=52.58.90.28
+		netsh advfirewall firewall add rule name="AllowIP1" dir=out action=allow remoteip=3.64.93.33
+		netsh advfirewall firewall add rule name="AllowIP2" dir=out action=allow remoteip=3.64.40.49
+		netsh advfirewall firewall add rule name="AllowIP3" dir=out action=allow remoteip=3.64.111.138
 	)
 ) else (
 	if "%1"=="unblock" (
@@ -29,8 +27,6 @@ if "%1"=="block" (
 			netsh advfirewall firewall del rule name="AllowIP1"
 			netsh advfirewall firewall del rule name="AllowIP2"
 			netsh advfirewall firewall del rule name="AllowIP3"
-			netsh advfirewall firewall del rule name="AllowIP4"
-			netsh advfirewall firewall del rule name="AllowIP5"
 			netsh advfirewall firewall set rule all new enable=yes
 		)
 	) else (
